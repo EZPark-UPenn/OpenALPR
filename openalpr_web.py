ca93e0c2617e5fa0ef77ca7d54b4bd888ac4ce42
Utkarsh Shah <utkarshshah007@gmail.com>
2016-02-18 22:15:16 +0000

    webserver

--- openalpr_web.py
+++ openalpr_web.py
@@ -1,36 +1,61 @@
+from flask import Flask, jsonify, request
+import json
+import urllib
+from base64 import b64encode
+import requests
+
 from openalpr import Alpr
+import openalpr
+
+import time
 
 import json
-import tornado.ioloop
-import tornado.web
 
-alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-alpr.set_top_n(20)
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return "hello world!"
 
+@app.route('/alpr', methods = ['POST'])
+def alpr():
+    start_time = time.time();
+    try:
+        print 'recieved image, processing...'
 
-class MainHandler(tornado.web.RequestHandler):
-    def post(self):
+        alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
+        alpr.set_top_n(20)
 
-        if 'image' not in self.request.files:
-            self.finish('Image parameter not provided')
+        mid1_time = time.time();
 
-        fileinfo = self.request.files['image'][0]
-        jpeg_bytes = fileinfo['body']
+        if 'image' not in request.files:
+            print "image was not in files"
+            return 'Image parameter not provided'
+
+        jpeg_bytes = request.files['image'].read()
 
         if len(jpeg_bytes) <= 0:
+            print "there are no bytes!"
             return False
+
+        mid2_time = time.time();
 
         results = alpr.recognize_array(jpeg_bytes)
 
-        self.finish(json.dumps(results))
+        print "got results!"
 
+        end_time = time.time();
 
+        print("total_time: " + str(end_time-start_time));
+        print("alpr_time: " + str(mid1_time-start_time));
+        print("jpeg_time: " + str(mid2_time-mid1_time));
+        print("processing_time: " + str(end_time-mid2_time));
 
-application = tornado.web.Application([
-    (r"/alpr", MainHandler),
-])
+        return jsonify(results)
+    except Exception, e:
+        print e
+        raise e
+    
 
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    app.run(debug = True)
---
+++ openalpr_web.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/webservice')
+
+from openalpr_web import app as application
